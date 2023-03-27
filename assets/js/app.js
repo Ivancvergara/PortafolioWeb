e59eb8b7dce2cc7a3df8c38c67f2377822ecfbc2
Iván Cuevas <irving_ivan1906@hotmail.com>
2023-03-26 20:50:19 -0600

    update file

--- assets/js/app.js
+++ assets/js/app.js
@@ -45,6 +45,76 @@ function seleccionar(link) {
     x.className = "";
 }
 
+//Código para el buscador de perfiles de Github
+const btnSearch =  document.querySelector(".search-section a");
+const inputUser =  document.querySelector(".search-section input");
+const mainContainer =  document.querySelector(".main-wraper");
+const containerSection =  document.querySelector(".container-section");
+
+const url = "https://api.github.com/users/";
+
+window.onload = function buscarUser(e){
+    e.preventDefault();
+    callApiUser(inputUser.value);
+}
+
+async function callApiUser(user){
+    const userUrl = url + user;
+    const repoUrl = `${url}${user}/repos`;
+    try {
+        const data = await Promise.all([fetch(userUrl), fetch(repoUrl)]);
+        const dataUser = await data[0].json();
+        const dataRepo = await data[1].json();
+        mostrarData(dataUser);
+        mostrarRepos(dataRepo);
+    } catch (error) {
+        console.log(error);
+    }
+
+}
+
+function mostrarData(dataUser){
+    clearHTML();
+    const {avatar_url, bio, followers, following, name, public_repos} = dataUser;
+    const container = document.createElement("div");
+    container.innerHTML = `
+        <div class="row-left">
+            <img src="${avatar_url}" alt="user image">
+        </div>
+        <div class="row-right">
+            <h3>${name}</h3>
+            <p>${bio}</p>
+            <div class="stats-user">
+                <p>${followers} <span>Followers</span></p>
+                <p>${following} <span>Following</span></p>
+                <p>${public_repos} <span>Repos</span></p>
+            </div>
+            <h3>Repositorios:</h3>
+            <div class="link-repos"></div>
+        </div>
+    `;
+    containerSection.appendChild(container);
+}
+
+function mostrarRepos(repos){
+    const reposContainer = document.querySelector(".link-repos");
+    repos
+        .sort((a, b) => b.stargazers_count - a.stargazers_count)
+        .slice(0, 15)
+        .forEach(element => {
+            const link = document.createElement("a");
+            link.innerText = element.name;
+            link.href = element.html_url;
+            link.target = "_blank";
+            reposContainer.appendChild(link);
+        });
+}
+
+function clearHTML(){
+    containerSection.innerHTML = "";
+}
+
+
 //Creo las barritas de una barra particular identificada por su id
 function crearBarra(id_barra){
     for(i=0;i<=16;i++){
